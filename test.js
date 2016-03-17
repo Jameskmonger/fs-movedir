e4ee6bb4a168828c21226b56559b7a551c169af3
James Monger <jameskmonger@hotmail.co.uk>
2016-03-17 19:53:57 +0000

    Make first test

--- test.js
+++ test.js
@@ -1,3 +1,18 @@
 'use strict';
 
 const test = require('tape');
+const proxyquire = require('proxyquire');
+
+test('it should make a directory when moving "x" into "a"', t => {
+    t.plan(1);
+
+    let fs = {
+        mkdir: function (dir, cb) {
+            t.pass('mkdir called');
+        }
+    };
+
+    let mod = proxyquire('./index.js', {'fs': fs});
+
+    mod('x', 'a');
+});

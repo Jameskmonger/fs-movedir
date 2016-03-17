9b0b88448ba0ecdcea07a2a7bd805a996f728440
James Monger <jameskmonger@hotmail.co.uk>
2016-03-17 19:55:34 +0000

    Check new directory name again with different test case

--- test.js
+++ test.js
@@ -34,3 +34,21 @@ test('it should make a directory "a" when moving "x" into "a"', t => {
 
     mod('x', toDir);
 });
+
+test('it should make a directory "b" when moving "x" into "b"', t => {
+    t.plan(1);
+
+    let toDir = 'b';
+
+    let fs = {
+        mkdir: function (dir, cb) {
+            if (dir === toDir) {
+                t.pass('mkdir called');
+            }
+        }
+    };
+
+    let mod = proxyquire('./index.js', {'fs': fs});
+
+    mod('x', toDir);
+});

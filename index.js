f476d3b6909144edefd3ab1f3c2e313d06ed3c39
James Monger <jameskmonger@hotmail.co.uk>
2016-03-17 20:07:47 +0000

    Implement solution

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 (function(mod) {
 
-    mod.exports = function(from, into) {
+    var fs = require('fs');
+    var path = require('path');
 
+    mod.exports = function(from, into) {
+        var dest = path.join(into, from);
+
+        fs.mkdir(into, () => {
+
+        	fs.rename(from, dest, () => {
+
+                console.log("'" + from + "' renamed to '" + dest + "'");
+
+        	});
+
+        });
     };
 
 })(module);
